7130dc43d54ba65d7b9ba69d970d630c033974b5
webdev817 <developer@jorgeolaya.co>
2020-09-14 14:13:16 -0400

    referrals-update

--- server/models/referrals.model.js
+++ server/models/referrals.model.js
@@ -31,6 +31,13 @@ const ReferralsSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  client_email: {
+    type: String,
+    required: true,
+    unique: true,
+    // Regexp to validate emails with more strict rules as added in tests/users.js which also conforms mostly with RFC2822 guide lines
+    match: [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please enter a valid email'],
+  },
   client_gender: {
     type: String,
     required: true
@@ -56,6 +63,12 @@ const ReferralsSchema = new mongoose.Schema({
   },
   client_zipcode: {
     type: String,
+  },
+  client_contact_by_email: {
+    type: Boolean,
+  },
+  client_send_message: {
+    type: Boolean,
   },
   client_primary_phonenumber: {
     type: String,
