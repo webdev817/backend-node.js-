36e97adefaabbb787f31032ae6523781d718a1bd
webdev817 <developer@jorgeolaya.co>
2020-09-15 13:49:36 -0400

    fix-errors

--- server/models/referrals.model.js
+++ server/models/referrals.model.js
@@ -3,63 +3,63 @@ const mongoose = require('mongoose');
 const ReferralsSchema = new mongoose.Schema({
   refer_firstname: {
     type: String,
-    required: true
+    
   },
   refer_lastname: {
     type: String,
-    required: true
+    
   },
   refer_entity: {
     type: String,
-    required: true
+    
   },
   refer_email: {
     type: String,
-    required: true,
+
     unique: true,
     // Regexp to validate emails with more strict rules as added in tests/users.js which also conforms mostly with RFC2822 guide lines
     match: [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please enter a valid email'],
   },
   client_firstname: {
     type: String,
-    required: true
+    
   },
   client_middlename: {
     type: String,
   },
   client_lastname: {
     type: String,
-    required: true
+    
   },
   client_email: {
     type: String,
-    required: true,
+    
     unique: true,
     // Regexp to validate emails with more strict rules as added in tests/users.js which also conforms mostly with RFC2822 guide lines
     match: [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please enter a valid email'],
   },
   client_gender: {
     type: String,
-    required: true
+    
   },
   client_birth: {
     type: Date,
-    required: true,
+
   },
   client_address1: {
     type: String,
-    required: true,
+
   },
   client_address2: {
     type: String,
   },
   client_city: {
     type: String,
-    required: true,
+
   },
   client_state: {
     type: String,
-    required: true,
+
   },
   client_zipcode: {
     type: String,
